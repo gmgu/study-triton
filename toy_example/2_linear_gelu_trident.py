06b97862d5b5ce09e2d9b18ca242643afaad7886
Geonmo <gmgu@ubuntu-20.04>
2023-09-13 19:14:51 +0900

    add 2_linear_gelu

---
+++ toy_example/2_linear_gelu_trident.py
@@ -0,0 +1,33 @@
+import trident
+import torch
+import torch.nn as nn
+import time
+
+
+device = torch.device('cuda:0')
+
+class LinearModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layer = trident.Linear(10, 1)
+        self.act = trident.GELU()
+    
+    def forward(self, x):
+        x = self.layer(x)
+        x = self.act(x)
+
+        return x
+
+
+model = LinearModel().to(device)
+
+data_x = torch.Tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]).to(device)
+
+start_time = time.time()
+for _ in range(1_000_000):
+    pred_y = model(data_x)
+end_time = time.time()
+
+print(pred_y)
+print(f'Elapsed Time: {end_time - start_time:.2f} sec')
